12c81e1b520ec39902fba1bd846524f0a91df367
aditya051312 <adityakhillari1@gmail.com>
2024-02-06 22:39:23 +0530

    Delete Dashboard.test.js

--- Dashboard.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Dashboard from './Dashboard';
-
-test('renders dashboard', () => {
-  const { getByText } = render(<Dashboard />);
-  expect(getByText('Welcome to the Dashboard!')).toBeInTheDocument();
-});
