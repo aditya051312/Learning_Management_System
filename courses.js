31025c1d822d18b7f91d180b3a1032796cec4851
aditya051312 <adityakhillari1@gmail.com>
2024-03-21 17:23:37 +0530

    Added courses.js for the Courses component

---
+++ courses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Courses = () => {
+  return (
+    <div className="p-6">
+      <h2 className="text-xl font-semibold text-gray-800">Courses</h2>
+      {/* Here you can add the course cards */}
+    </div>
+  );
+};
+
+export default Courses;
