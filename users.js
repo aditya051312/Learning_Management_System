5ce69607fda266627618f3bee943532cc6cceea7
aditya051312 <adityakhillari1@gmail.com>
2024-03-21 17:24:07 +0530

    Added users.js for the Users component

---
+++ users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Users = () => {
+  return (
+    <div className="p-6">
+      <h2 className="text-xl font-semibold text-gray-800">Users</h2>
+      {/* Here you can add the user list */}
+    </div>
+  );
+};
+
+export default Users;
