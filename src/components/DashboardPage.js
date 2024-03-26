7ea2fad7522a2a4f573e67edf3b351ebdcc3eb87
aditya051312 <adityakhillari1@gmail.com>
2024-03-26 22:15:41 +0530

    Added DashboardPage component

---
+++ src/components/DashboardPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class DashboardPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        <p>Welcome to your dashboard!</p>
+      </div>
+    );
+  }
+}
+
+export default DashboardPage;
